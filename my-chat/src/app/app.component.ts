def632542171210f592a36389b2125a96ab93739
Chu Hiển Đạt <chuhiendat@Chus-Laptop.local>
2023-09-07 06:33:25 +1000

    create user & auth services, and implement user service

--- my-chat/src/app/app.component.ts
+++ my-chat/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './services/user.service';  // 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-chat';
+export class AppComponent implements OnInit {
+
+  title = 'Your Angular App';
+
+  constructor(private userService: UserService) {  // Inject the UserService
+
+  }
+
+  ngOnInit(): void {
+    // Initialize the default users via UserService
+    this.userService.initializeUsers();
+  }
 }
--- my-chat/src/app/services/user.service.ts
+++ my-chat/src/app/services/user.service.ts
@@ -6,4 +6,37 @@ import { Injectable } from '@angular/core';
 export class UserService {
 
   constructor() { }
+
+  // Initialize users if they don't exist
+  initializeUsers(): void {
+    if (!localStorage.getItem('users')) {
+      const users = [
+        { username: 'test_sp', password: '123', role: 'super_admin', groups: [] },
+        { username: 'test_ga', password: '123', role: 'group_admin', groups: [] },
+        { username: 'test_user', password: '123', role: 'user', groups: [] }
+      ];
+      localStorage.setItem('users', JSON.stringify(users));
+    }
+  }
+
+  // Get all users
+  getUsers(): any[] {
+    return JSON.parse(localStorage.getItem('users') || '[]');
+  }
+
+  // Add new user
+  addUser(user: any): void {
+    const users = this.getUsers();
+    users.push(user);
+    localStorage.setItem('users', JSON.stringify(users));
+  }
+
+  // Check role of a user
+  checkRole(username: string): string | null {
+    const users = this.getUsers();
+    const user = users.find(u => u.username === username);
+    return user ? user.role : null;
+  }
+
+  // Your additional methods to handle user functionalities can go here
 }
