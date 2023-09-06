c541f53ccb8a9ed7cfa3a8f479dfbbe300b58507
Chu Hiển Đạt <chuhiendat@Chus-Laptop.local>
2023-09-07 07:18:08 +1000

    make simple authentication using ngIfElse

--- my-chat/src/app/app.component.ts
+++ my-chat/src/app/app.component.ts
@@ -1,20 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService } from './services/user.service';  // 
+import { UserService } from './services/user.service';  // Make sure the path is correct
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-
-  title = 'Your Angular App';
+  title = 'Workplace Chat';
+  
+  public isAuthenticated: boolean = false;  // New property for tracking authentication
+  public userRole: string | null = null;  // New property for tracking the user role
 
   constructor(private userService: UserService) {  // Inject the UserService
-
+    // Initialize the default users via UserService
+    this.userService.initializeUsers();
   }
 
   ngOnInit(): void {
-    // Initialize the default users via UserService
-    this.userService.initializeUsers();
+    // Get the current user
+    const currentUser = this.userService.getCurrentUser();  
+
+    // Check if the user is authenticated
+    if (currentUser) {
+      this.isAuthenticated = true;
+      this.userRole = currentUser.role;
+    }
   }
 }
--- my-chat/src/app/services/user.service.ts
+++ my-chat/src/app/services/user.service.ts
@@ -24,11 +24,32 @@ export class UserService {
     return JSON.parse(localStorage.getItem('users') || '[]');
   }
 
+  // Get current user
+  getCurrentUser(): any | null {
+    return JSON.parse(localStorage.getItem('currentUser') || 'null');
+  }
+
   // Add new user
   addUser(user: any): void {
     const users = this.getUsers();
     users.push(user);
     localStorage.setItem('users', JSON.stringify(users));
+  }
+
+  // Login
+  login(username: string, password: string): boolean {
+    const users = this.getUsers();
+    const user = users.find(u => u.username === username && u.password === password);
+    if (user) {
+      localStorage.setItem('currentUser', JSON.stringify(user));
+      return true;
+    }
+    return false;
+  }
+
+  // Logout
+  logout(): void {
+    localStorage.removeItem('currentUser');
   }
 
   // Check role of a user
@@ -37,6 +58,4 @@ export class UserService {
     const user = users.find(u => u.username === username);
     return user ? user.role : null;
   }
-
-  // Your additional methods to handle user functionalities can go here
 }
